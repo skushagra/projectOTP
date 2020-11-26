b4aeb47ae00bc3c7c2bced30439bf380143e066d
Kushagra S <kushagra.rigel@gmail.com>
2020-11-26 23:49:44 +0530

    Updated send.py
    send.py has been updated to now send messages to people . For more details see readme.md file

--- send.py
+++ send.py
@@ -4,11 +4,15 @@ import json
 def send_sms(number, message):
 	url = 'https://www.fast2sms.com/dev/bulk'
 	para = {
-	'authorization' = '0akrzRopPTJV9OHENKxfyMctuZeqUbSG7DdF6Xg1C3IQ8BAhYm0snNVvCpIWU4S9YBj7M85qQkDt2yzF'
+	'authorization' = '# Your Fast2SMS authorization key'
 	'sender_id' = 'FSTSMS'
 	'language' = 'english'
 	'route' = 'p'
-	'message' = "This is a test flash message!"
-	'numbers' = '7018487271'
+	'message' = message
+	'numbers' = number
 	'flash' = "1"
 	}
+
+number = input('Enter your number : ')
+message = input('Enter your message : ')
+send_sms(number, message)
